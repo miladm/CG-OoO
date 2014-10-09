cceb64730b56909758b861976a45428351529345
Milad Mohammadi <milad@alumni.stanford.edu>
2014-10-08 17:43:36 -0700

    addd the verification framework for autmating basic results verification

--- src/verif/result_sanity_check.sh
+++ src/verif/result_sanity_check.sh
@@ -1,4 +1,11 @@
 RESULT_PATH='/scratch/milad/qsub_outputs/perf_sim_test/out1'
 
 cd $RESULT_PATH
+echo "============================="
+echo "CHCECK INSTRUCTION COUNTS"
 grep "commit.ins_cnt" 4*
+
+echo "============================="
+echo "CHCECK INSTRUCTION COUNTS"
+grep "CHECK FILE TIMESTAMPS"
+llt 4*
