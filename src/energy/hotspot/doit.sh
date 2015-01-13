d27bd5de74e50a8debdc4c3d85cf4b8861baac83
Milad Mohammadi <milad@alumni.stanford.edu>
2015-01-13 15:08:11 -0800

    added the input hotspot input files for o3 and ino - update the bb input files

---
+++ src/energy/hotspot/doit.sh
@@ -0,0 +1,3 @@
+./hotfloorplan -f input/bb.desc -p input/bb.p -o output/bb.out
+#./hotfloorplan -f input/ino.desc -p input/ino.p -o output/ino.out
+#./hotfloorplan -f input/o3.desc -p input/o3.p -o output/o3.out
