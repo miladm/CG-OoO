8aedf551e9eb86151d00e3721c11fdedf5774939
Milad Mohammadi <milad@alumni.stanford.edu>
2015-02-15 14:11:10 -0800

    added more elements to the fetch_results script

--- analysis/remote_access/fetch_results.sh
+++ analysis/remote_access/fetch_results.sh
@@ -3,3 +3,4 @@ RESULTS_DIR="/scratch/milad/qsub_outputs/perf_sim_test"
 
 EXP=multi_bb_fetch/backend_cfg/bb1
 scp $SERVER:$RESULTS_DIR/$EXP/analysis/commit.ipc .
+scp $SERVER:$RESULTS_DIR/$EXP/analysis/TOTAL.ipc .
