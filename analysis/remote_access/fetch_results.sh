c5532c8b0e246024f847c3d77e10d016e04205f9
Milad Mohammadi <milad@alumni.stanford.edu>
2015-02-15 14:18:43 -0800

    small changes

--- analysis/remote_access/fetch_results.sh
+++ analysis/remote_access/fetch_results.sh
@@ -1,6 +1,7 @@
-SERVER=milad@snooki.stanford.edu
+SERVER="milad@snooki.stanford.edu"
 RESULTS_DIR="/scratch/milad/qsub_outputs/perf_sim_test"
+EXT=".csv"
 
 EXP=multi_bb_fetch/backend_cfg/bb1
-scp $SERVER:$RESULTS_DIR/$EXP/analysis/commit.ipc .
-scp $SERVER:$RESULTS_DIR/$EXP/analysis/TOTAL.ipc .
+scp $SERVER:$RESULTS_DIR/$EXP/analysis/commit.ipc$EXT .
+scp $SERVER:$RESULTS_DIR/$EXP/analysis/TOTAL$EXT .
