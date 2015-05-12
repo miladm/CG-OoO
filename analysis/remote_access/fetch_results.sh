bf86bb03c9cf2af2bb2c6e429ec097f8c521400a
Milad <milmil@Mili-iMac.local>
2015-05-12 09:32:22 -0700

    adding a  new fetch script

--- analysis/remote_access/fetch_results.sh
+++ analysis/remote_access/fetch_results.sh
@@ -1,7 +1,14 @@
 SERVER="milad@snooki.stanford.edu"
-RESULTS_DIR="/scratch/milad/qsub_outputs/perf_sim_test"
+RESULTS_DIR="/scratch/milad/qsub_outputs/perf_sim_test/"
 EXT=".csv"
 
-EXP=multi_bb_fetch/backend_cfg/bb1
-scp $SERVER:$RESULTS_DIR/$EXP/analysis/commit.ipc$EXT .
-scp $SERVER:$RESULTS_DIR/$EXP/analysis/TOTAL$EXT .
+#EXP="multi_bb_fetch/width_size/width_1/o3_ino"
+#EXP="multi_bb_fetch/width_size/width_1/bb"
+EXP='multi_bb_fetch/perfect_mem/o3_ino_w1'
+
+
+FILE="commit.ipc"
+scp $SERVER:$RESULTS_DIR/$EXP/analysis/$FILE$EXT .
+
+FILE='Energy'
+scp "$SERVER:$RESULTS_DIR/$EXP/analysis/$FILE$EXT" .
